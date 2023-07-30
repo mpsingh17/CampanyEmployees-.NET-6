fb0e2ab4cc8612e3828944532688a24d989475ef
Manpreet <mpsingh17@outlook.co.nz>
2023-07-30 14:59:44 +1200

    Repository pattern set up. Base repo created.

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,38 @@
+﻿using Contracts;
+using Entities;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext RepositoryContext;
+
+        public RepositoryBase(RepositoryContext repositoryContext)
+        {
+            RepositoryContext = repositoryContext;
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            trackChanges
+            ? RepositoryContext.Set<T>()
+            : RepositoryContext.Set<T>().AsNoTracking();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> condition, bool trackChanges) =>
+            trackChanges
+            ? RepositoryContext.Set<T>().Where(condition)
+            : RepositoryContext.Set<T>().AsNoTracking().Where(condition);
+
+        public void Create(T entity) =>
+            RepositoryContext.Set<T>().Add(entity);
+
+        public void Delete(T entity) =>
+            RepositoryContext.Set<T>().Remove(entity);
+
+        public void Update(T entity) =>
+            RepositoryContext.Set<T>().Update(entity);
+    }
+}
