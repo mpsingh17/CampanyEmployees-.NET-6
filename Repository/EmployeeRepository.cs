6367e13b2931fbfea6dcc0099efefa595c5eed24
Manpreet <mpsingh17@outlook.co.nz>
2023-09-10 18:25:31 +1200

    Implement age filter in employee respository.

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -42,11 +42,16 @@ namespace Repository
             EmployeeParameters employeeParameters, 
             bool trackChanges)
         {
-            var employees =  await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
-                .OrderBy(e => e.Name)
-                .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
-                .Take(employeeParameters.PageSize)
-                .ToListAsync();
+            var employees =  await FindByCondition(
+                e => e.CompanyId.Equals(companyId) &&
+                e.Age >= employeeParameters.MinAge &&
+                e.Age <= employeeParameters.MaxAge,
+                trackChanges
+            )
+            .OrderBy(e => e.Name)
+            .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
+            .Take(employeeParameters.PageSize)
+            .ToListAsync();
 
             var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
                 .CountAsync();
