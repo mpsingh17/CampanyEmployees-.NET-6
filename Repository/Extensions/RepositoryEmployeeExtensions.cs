08c8d90dc8c45db43967f4f375e47458c539d0e7
Manpreet <mpsingh17@outlook.co.nz>
2023-09-12 12:25:23 +1200

    Extension method to filter and search employees.

---
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Extensions
+{
+    public static class RepositoryEmployeeExtensions
+    {
+        public static IQueryable<Employee> FilterEmployees(
+            this IQueryable<Employee> employees,
+            uint minAge,
+            uint maxAge)
+        {
+            return employees.Where(emp => emp.Age >= minAge && emp.Age <= maxAge);
+        }
+
+        public static IQueryable<Employee> Search(
+            this IQueryable<Employee> employees, 
+            string searchTerm)
+        {
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                return employees;
+            }
+
+            var sanitisedSearchTerm = searchTerm.Trim().ToLower();
+
+            return employees.Where(emp => emp.Name.ToLower().Contains(sanitisedSearchTerm));
+        }
+    }
+}
